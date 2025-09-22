00f20b9edcc1dae7717191d8b006790f252d7517
brenonun3s <morais.brenonunes@hotmail.com>
2025-09-22 08:53:19 -0300

    feat(ForClassico): add código de implementação com estudos de For classico

---
+++ estruturas_repeticao/ForClassico.js
@@ -0,0 +1,10 @@
+const frutas = ['Maça', 'Pera', 'Uva', 'Laranja'];
+
+for (let i = 0; i < frutas.length; i++) {
+ console.log(frutas[i]);
+}
+
+for (let i = 0; i <= 10; i++) {
+ const par =  i % 2 === 0 ? 'par' : 'impar';
+ console.log(i, par);
+}
